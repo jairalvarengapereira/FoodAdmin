475d0e69dc048f7f147fbc077bc21f4af2cf7e9b
jair-ap <66567980+jairalvarengapereira@users.noreply.github.com>
2024-03-13 22:22:10 -0300

    13/03/2024 22:22

--- FoodAdmin.dpr
+++ FoodAdmin.dpr
@@ -3,12 +3,14 @@ program FoodAdmin;
 uses
   System.StartUpCopy,
   FMX.Forms,
-  UnitLogin in 'UnitLogin.pas' {FrmLogin};
+  UnitLogin in 'UnitLogin.pas' {FrmLogin},
+  UnitPrincipal in 'UnitPrincipal.pas' {FrmPrincipal};
 
 {$R *.res}
 
 begin
   Application.Initialize;
+  Application.CreateForm(TFrmPrincipal, FrmPrincipal);
   Application.CreateForm(TFrmLogin, FrmLogin);
   Application.Run;
 end.
